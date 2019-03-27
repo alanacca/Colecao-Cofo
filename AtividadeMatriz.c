9c025b09a4d1059df20cc72e65dea9fbd0a377ff
alanacca <44409612+alanacca@users.noreply.github.com>
2019-03-27 12:45:57 -0300

    Update and rename main.c to AtividadeMatriz.c

--- AtividadeMatriz.c
+++ AtividadeMatriz.c
@@ -1,28 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//3 QUESTﾃO
-float *DiagPrin(int n,int m, float **Ma)//MOSTRA A DIAGONAL PRINCIPAL DA MATRIZ
-{
-    float *vetDiag = (float*)malloc(sizeof(float)*n);
-    if(vetDiag == NULL){
-        printf("vetor nulo");
-    }
-    int s=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==j){
-                vetDiag[s]=Ma[i][j];
-                s++;
-
-            }
-        }
-    }
-    return vetDiag;
-}
-
-
-//1 QUESTﾃO
+//1 QUESTﾃグ
+/*Faﾃｧa um algoritmo que recebe dois nﾂｺ inteiros n e m, 
+cria uma matriz de floats de tamanho nxm, lﾃｪ todos os elementos da matriz e retorn a matriz preenchida.*/
 float **criaEleMatriz(int n,int m){//CRIA UMA MATRIZ
     float **M = (float**)malloc(sizeof(float*)*n);
     for(int i=0;i<n;i++){
@@ -40,64 +21,10 @@ float **criaEleMatriz(int n,int m){//CRIA UMA MATRIZ
     return M;
 }
 
-
-void impMatriz(float**M,int n,int m){//IMPRIME A MATRIZ
-    if(M!=NULL){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                printf("matriz:%0.2f \t",M[i][j]);
-            }
-            printf("\n");
-        }
-    }
-}
-
-//4 QUESTﾃO LETRA A
-float *RetornaLinha(int n,int m,float **Mat, int k){//RETORNA A LINHA K
-    float *vetor = (float*)malloc(sizeof(float)*m);
-    if(Mat!=NULL && k<n && k>=0){
-        for(int i=0;i<m;i++){
-            vetor[i] = Mat[k][i];
-        }
-    }
-    return vetor;
-}
-
-//4 QUESTﾃO LETRA B
-float *RetornaColuna(int n,int m, float **Mc, int k){//RETORNA A COLUNA K
-    float *vet = (float*)malloc(sizeof(float)*n);
-    if(Mc!=NULL && k<m&& k>=0){
-        for(int i=0;i<n;i++){
-            vet[i] = Mc[i][k];
-        }
-    }
-    return vet;
-}
-
-//4 QUESTﾃO LETRA C
-float RetornaSomaEleColunaK(int n,int m,float **Ma,int k){//RETORNA A SOMA DA COLUNA K
-    float somaColuna=0;
-    if(Ma!=NULL){
-        for(int i=0;i<n;i++){
-            somaColuna+=Ma[i][k];
-        }
-    }
-    return somaColuna;
-}
-
-//4 QUESTﾃO LETRA D
-float RetornaSomaElemLinhaK(int n,int m,float **Ma,int k){//RETORNA A SOMA DA LINHA K
-    float somaLinha=0;
-    if(Ma!=NULL){
-       for(int i=0;i<m;i++){
-            somaLinha+=Ma[k][i];
-       }
-    }
-    return somaLinha;
-}
-
-//2 QUESTﾃO
-float **multMat(int n,int m,int p, int f,float **Ma,float **Mb){//FAZ A MULTIPLICAﾇﾃO DA MATRIZ
+//2 QUESTﾃグ
+/*Faﾃｧa um algoritmo que recebe 4 nﾃｺmeros inteiros n,m,p e f, e duas matrizes floats MAnxm e MBpxf
+e retorna a matriz MC com o resultado da multiplicaﾃｧﾃ｣o MC=MAxMB se possﾃｭvel ou "NULL" em caso contrﾃ｡rio*/
+float **multMat(int n,int m,int p, int f,float **Ma,float **Mb){//FAZ A MULTIPLICAﾃ�ﾃグ DA MATRIZ
     if(m == p){
         float **matriz = (float**)malloc(sizeof(float*)*n);
             for(int i=0;i<n;i++){
@@ -117,6 +44,92 @@ float **multMat(int n,int m,int p, int f,float **Ma,float **Mb){//FAZ A MULTIPLI
     }
     return NULL;
 }
+
+//3 QUESTﾃグ
+/*Faﾃｧa um algoritmo que recebe dois inteiros n e m e uma matriz MAnxm e
+retorna um vetor com os elementos da diagonal principal*/
+float *DiagPrin(int n,int m, float **Ma)//MOSTRA A DIAGONAL PRINCIPAL DA MATRIZ
+{
+    float *vetDiag = (float*)malloc(sizeof(float)*n);
+    if(vetDiag == NULL){
+        printf("vetor nulo");
+    }
+    int s=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==j){
+                vetDiag[s]=Ma[i][j];
+                s++;
+
+            }
+        }
+    }
+    return vetDiag;
+}
+
+void impMatriz(float**M,int n,int m){//IMPRIME A MATRIZ
+    if(M!=NULL){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                printf("matriz:%0.2f \t",M[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+//4 QUESTﾃグ
+/*Faﾃｧa um algoritmo que recebe dois inteiros n e m e uma matriz MAnxm e um inteiro k*/
+//LETRA A
+/*Retorna um vetor com elementos da linha k */
+
+float *RetornaLinha(int n,int m,float **Mat, int k){//RETORNA A LINHA K
+    float *vetor = (float*)malloc(sizeof(float)*m);
+    if(Mat!=NULL && k<n && k>=0){
+        for(int i=0;i<m;i++){
+            vetor[i] = Mat[k][i];
+        }
+    }
+    return vetor;
+}
+
+//4 QUESTﾃグ LETRA B
+/*Retorna um vetor com elementos da coluna k*/
+float *RetornaColuna(int n,int m, float **Mc, int k){//RETORNA A COLUNA K
+    float *vet = (float*)malloc(sizeof(float)*n);
+    if(Mc!=NULL && k<m&& k>=0){
+        for(int i=0;i<n;i++){
+            vet[i] = Mc[i][k];
+        }
+    }
+    return vet;
+}
+
+//4 QUESTﾃグ LETRA C
+/*Retorna a soma dos elementos da coluna k*/
+float RetornaSomaEleColunaK(int n,int m,float **Ma,int k){//RETORNA A SOMA DA COLUNA K
+    float somaColuna=0;
+    if(Ma!=NULL){
+        for(int i=0;i<n;i++){
+            somaColuna+=Ma[i][k];
+        }
+    }
+    return somaColuna;
+}
+
+//4 QUESTﾃグ LETRA D
+/*Retorna a soma dos elementos da linha k*/
+float RetornaSomaElemLinhaK(int n,int m,float **Ma,int k){//RETORNA A SOMA DA LINHA K
+    float somaLinha=0;
+    if(Ma!=NULL){
+       for(int i=0;i<m;i++){
+            somaLinha+=Ma[k][i];
+       }
+    }
+    return somaLinha;
+}
+
+
 int main()
 {
     int n,m,p,f,k;
@@ -145,17 +158,17 @@ int main()
     MatrizResul = multMat(n,m,p,f,MatrizA,MatrizB);
     impMatriz(MatrizResul,n,f);
 
-    /*float *v;
+    float *v;
     v = RetornaColuna(n,m,recebeMatriz,k);
     for(int i=0;i<n;i++){
         printf("Coluna:%0.2f \t",v[i]);
-    }*/
+    }
     //printf("\n");
     //printf("somaColuna:%f\n",RetornaSomaEleColunaK(n,m,recebeMatriz,k));
     //printf("soma da linha:%0.2f\n",RetornaSomaElemLinhaK(n,m,recebeMatriz,k));
 
 }
-   /*srand(time(NULL));
+   srand(time(NULL));
    int n,m;
    printf("Digite a quantidade de linhas:\n");
    scanf("%d",&n);
@@ -191,6 +204,6 @@ int main()
         for(int i=0;i<m;i++){
             printf("vetor: %f\n",vetor[i]);
         }
-    }*/
+    }
 
 
